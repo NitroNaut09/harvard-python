cb620483613797653160fe11fb991c9272ffd255
Deeraj <hrishikeshdeeraj@gmail.com>
2024-03-16 22:38:41 +0530

    Trying to do CS50 tasks

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 # Input takes an argument itself
 
+
 def app():
     """
     A function that prompts the user to enter something and then prints it.
     """
     print(input("Enter something: "))
+
+
 app()
 
-#Parameters to the functions.
+# Parameters to the functions.
 
-#For print function:
-    print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+# For print function:
+print(*objects, sep=" ", end="\n", file=sys.stdout, flush=False)
 
-'''  * means to multiply, and object means the string itself. Since there is no value for objects, it means that the print functions can take any number of strings.
-    
-    Separator is used to separate the objects in the string. The default value is ' ' (a space).
+"""  * means to multiply, and object means the string itself. Since there is no value for objects, it means that the print functions can take any number of strings.
 
-    End is used to end the string. The default value is '\n' (a newline).
+Separator is used to separate the objects in the string. The default value is ' ' (a space).
 
-    File is used to specify the file to which the output should be written. The default value is sys.stdout.
+End is used to end the string. The default value is '\n' (a newline).
 
-    Flush is used to flush the output buffer. The default value is False.
-        '''
+File is used to specify the file to which the output should be written. The default value is sys.stdout.
+
+Flush is used to flush the output buffer. The default value is False.
+"""
 
 # For example, we can modify the values of the parameters of the print function as follows:
 
-print(1, 2, 3, sep='--')
+print(1, 2, 3, sep="--")
 
 # IF we insist on using double quotes but still want to print quotes, we can use an escape character. For eg;
 
-print("I am \"fine\"")
+print('I am "fine"')
 
 
 # Example code:
 
-    name = input("Enter your name: ")
-    name = name.strip().title()
-    print(name)
+name = input("Enter your name: ")
+name = name.strip().title()
+print(name)
+
+# Conditional Statements
